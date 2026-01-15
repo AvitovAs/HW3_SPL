bfa2ed0b17262e5fef718ecb66d43bb6bd368710
harel12381 <harelhad100@gmail.com>
2026-01-15 00:29:11 +0000

    compelte ConnectionImpl

--- server/src/main/java/bgu/spl/net/srv/ConnectionImpl.java
+++ server/src/main/java/bgu/spl/net/srv/ConnectionImpl.java
@@ -1,21 +1,22 @@
 package bgu.spl.net.srv;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ConnectionImpl<T> implements Connections<T> {
     private ConcurrentHashMap<Integer, ConnectionHandler<T>> conconnections;
-    private ConcurrentHashMap<String, Channel> channels;
-    private ConcurrentHashMap<Integer, ConcurrentArrayList<String>> userChannels;
+    private ConcurrentHashMap<String, CopyOnWriteArrayList<Integer>> channels;
+    private ConcurrentHashMap<Integer, CopyOnWriteArrayList<String>> userChannels;
 
     public ConnectionImpl (){
         conconnections = new ConcurrentHashMap<Integer, ConnectionHandler<T>>();
-        channels = new ConcurrentHashMap<String, Channel>();
-        userChannels = new ConcurrentHashMap<Integer, ConcurrentArrayList<String>>();
+        channels = new ConcurrentHashMap<String, CopyOnWriteArrayList<Integer>>();
+        userChannels = new ConcurrentHashMap<Integer, CopyOnWriteArrayList<String>>();
     }
 
     @Override
     public boolean send(int connectionId, T msg) {
         ConnectionHandler<T> handler = conconnections.get(connectionId);
-        if (handler != null) {
+        if (handler != null){
             handler.send(msg);
             return true;
         }
@@ -24,52 +25,63 @@ public class ConnectionImpl<T> implements Connections<T> {
 
     @Override
     public void send(String channel, T msg) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'send'");
-
+        channels.computeIfPresent(channel, (k, subs) -> 
+        { 
+            for (Integer connectionId : subs){
+                send(connectionId, msg);
+            }
+            return subs;
+        });
     }
 
     @Override
     public void disconnect(int connectionId) {
-        conconnections.remove(connectionId);
-
-        ConcurrentArrayList<String> userChs = userChannels.get(connectionId);
-        if (userChs != null) {
-            synchronized (userChs) {
-                for (String channelName : userChs.getSnapshot()) {
-                    Channel ch = channels.get(channelName);
-                    if (ch != null) 
-                        ch.unsubscribe(connectionId);
-                    userChannels.remove(connectionId);
-                }
+        userChannels.computeIfPresent(connectionId, (k, channelList) -> 
+        { 
+            for (String channel : channelList){
+                channels.computeIfPresent(channel, (ch, subs) -> 
+                { 
+                    subs.remove(Integer.valueOf(connectionId)); 
+                    if (subs.isEmpty()) 
+                        return null; 
+                    else 
+                        return subs;
+                });
             }
-        }
+            return null;
+        });
     }
 
     @Override
     public void connect(int connectionId, ConnectionHandler<T> handler) {
         conconnections.putIfAbsent(connectionId, handler);
-        userChannels.putIfAbsent(connectionId, new ConcurrentArrayList<String>());
     }
 
     @Override
     public void subscribe(int connectionId, String channel) {
-        channels.putIfAbsent(channel, new Channel(channel));
-        Channel ch = channels.get(channel);
-
-        ch.subscribe(connectionId);
-        userChannels.get(connectionId).add(channel);
+        channels.computeIfAbsent(channel, (k) -> new CopyOnWriteArrayList<Integer>()).add(connectionId);
+        userChannels.computeIfAbsent(connectionId, (k) -> new CopyOnWriteArrayList<String>()).add(channel);
     }
 
     @Override
     public void unsubscribe(int connectionId, String channel) {
-        Channel ch = channels.get(channel);
-        if (ch != null)
-            synchronized (ch) {
-                ch.unsubscribe(connectionId);
-                userChannels.get(connectionId).remove(channel);
-                if (ch.getMembersCount() == 0)
-                    channels.remove(channel);
-            }
+        channels.computeIfPresent(channel, (k, subs) -> 
+        { 
+            subs.remove(Integer.valueOf(connectionId)); 
+            if (subs.isEmpty()) 
+                return null; 
+            else 
+                return subs;
+        });
+
+        userChannels.computeIfPresent(connectionId, (k, channels) -> 
+        { 
+            channels.remove(channel); 
+            if (channels.isEmpty()) 
+                return null; 
+            else 
+                return channels;
+        });
+        
     }
 }
