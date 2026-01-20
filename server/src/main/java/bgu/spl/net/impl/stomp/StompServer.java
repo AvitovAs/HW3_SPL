cfa4eb2a17a3bf2051cf641d65a1b49d9d57c10c
Asaf Yosef Avitov <asafraf99@gmail.com>
2026-01-20 21:29:14 +0200

    Add shutdown hook to print database report

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,5 +1,6 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.impl.data.Database;
 import bgu.spl.net.srv.Server;
 
 public class StompServer {
@@ -9,26 +10,27 @@ public class StompServer {
             System.out.println("Usage: java StompServer <port> <mode>");
             return;
         }
-        
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutdown Hook Triggered: Printing Report...");
+            Database.getInstance().printReport();
+        }));
+
         String mode = args[1];
         int port = Integer.parseInt(args[0]);
 
-        if (mode.equals("TPC")) {           
+        if (mode.equals("TPC")) {
             Server.threadPerClient(
-                port,
-                StompMessagingProtocolImpl::new,
-                StompEncoderDecoderImpl::new
-            ).serve();
-        }
-        else if (mode.equals("REACTOR")) {
+                    port,
+                    StompMessagingProtocolImpl::new,
+                    StompEncoderDecoderImpl::new).serve();
+        } else if (mode.equals("REACTOR")) {
             Server.reactor(
-                Runtime.getRuntime().availableProcessors(),
-                port,
-                StompMessagingProtocolImpl::new,
-                StompEncoderDecoderImpl::new
-            ).serve();
-        }
-        else{
+                    Runtime.getRuntime().availableProcessors(),
+                    port,
+                    StompMessagingProtocolImpl::new,
+                    StompEncoderDecoderImpl::new).serve();
+        } else {
             System.out.println("Invalid mode. Use 'TPC' or 'REACTOR'.");
         }
     }
